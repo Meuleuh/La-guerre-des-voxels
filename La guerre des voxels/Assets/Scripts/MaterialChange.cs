f19bef987614c3c655d70e691d1b3765382e70e8
Jean-Christophe Groleau <jcgroleau@gmail.com>
2018-01-28 20:47:58 -0500

    Pre-Build 1
    - Ajout de la chaussure
    - Ajout de 252 teintes pour la chaussure
    - Ajout du script pour changer la texture d'un objet

---
+++ La guerre des voxels/Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    [SerializeField]
+    Texture Texture;
+
+    Renderer Renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Texture Loaded Sucessfully...");
+        Renderer.material.mainTexture = Texture;
+    }
+}
