7e4940bc9e38b0f8c51eb8c17ef0abcb4d64a18d
Jean-Christophe Groleau <jcgroleau@gmail.com>
2018-02-02 20:35:58 -0500

    Pre-Build 1.1.0
    Un Pre-Build majeur qui me rapelle que ça prend deux points et non pas juste deux chiffres à la fin
    
    Ajouts:
    - Système externe pour modifier les couleurs des différentes parties
    - Palettes de couleur en blanc et gris pâle pour l'ombrage lorsque nécessaire (Ex: Lacets)
    
    Modifications:
    - Le modèle de la chaussure est maintenant en parties pour permettre l'optimisation de la coloration
    
    Suppression:
    - Les fichiers de palettes de textures étant désormais rendu désuets, ils ont été supprimés des fichers du projet

---
+++ La guerre des voxels/Assets/Scripts/SetSomethingElseColorExample.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSomethingElseColorExample : MonoBehaviour
+{
+
+    GameObject Semelle;
+    Renderer CouleurSemelle;
+
+    GameObject Lacets;
+    Renderer CouleurLacets;
+
+    GameObject CouleurPrincipale;
+    Renderer CouleurCouleurPrincipale;
+
+    GameObject DetailCote;
+    Renderer CouleurDetailCote;
+
+    GameObject DetailAvant;
+    Renderer CouleurDetailAvant;
+
+    GameObject DetailArriere;
+    Renderer CouleurDetailArriere;
+
+    [SerializeField]
+    Color CouleurPourSemelle;
+
+    [SerializeField]
+    Color CouleurPourLacets;
+
+    [SerializeField]
+    Color CouleurPourCouleurPrincipale;
+
+    [SerializeField]
+    Color CouleurPourDetailCote;
+
+    [SerializeField]
+    Color CouleurPourDetailAvant;
+
+    [SerializeField]
+    Color CouleurPourDetailArriere;
+
+    // Use this for initialization
+    void Start()
+    {
+        Semelle = GameObject.Find("Chaussure/Semelle");
+        CouleurSemelle = Semelle.GetComponent<Renderer>();
+        Lacets = GameObject.Find("Chaussure/Lacets");
+        CouleurLacets = Lacets.GetComponent<Renderer>();
+        CouleurPrincipale = GameObject.Find("Chaussure/CouleurPrincipale");
+        CouleurCouleurPrincipale = CouleurPrincipale.GetComponent<Renderer>();
+        DetailCote = GameObject.Find("Chaussure/DetailCote");
+        CouleurDetailCote = DetailCote.GetComponent<Renderer>();
+        DetailAvant = GameObject.Find("Chaussure/DetailAvant");
+        CouleurDetailAvant = DetailAvant.GetComponent<Renderer>();
+        DetailArriere = GameObject.Find("Chaussure/DetailArriere");
+        CouleurDetailArriere = DetailArriere.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CouleurSemelle.material.SetColor("_Color", CouleurPourSemelle);
+        CouleurLacets.material.SetColor("_Color", CouleurPourLacets);
+        CouleurCouleurPrincipale.material.SetColor("_Color", CouleurPourCouleurPrincipale);
+        CouleurDetailCote.material.SetColor("_Color", CouleurPourDetailCote);
+        CouleurDetailAvant.material.SetColor("_Color", CouleurPourDetailAvant);
+        CouleurDetailArriere.material.SetColor("_Color", CouleurPourDetailArriere);
+    }
+}
