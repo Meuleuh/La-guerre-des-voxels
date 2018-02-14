ddf063bdb07424cf96288aa5ce3d7a50736133c1
Jean-Christophe Groleau <jcgroleau@gmail.com>
2018-02-14 12:12:44 -0500

    Résultat du premier "Rush to the Alpha" du dimanche 11 février 2018
    Toutes les modifications créées durant le Rush to the Alpha du 11 février 2018 (durée du live: 5 heures, en ligne sur youtube sur ma chaîne personnelle du nom de Meuleuh en accéléré 20x)

--- La guerre des voxels/Assets/Scripts/ExempleDeTirSurLePistoletDeBase.cs
+++ La guerre des voxels/Assets/Scripts/ExempleDeTirSurLePistoletDeBase.cs
@@ -8,18 +8,65 @@ public class ExempleDeTirSurLePistoletDeBase : MonoBehaviour
     [SerializeField]
     GameObject Balle;
 
-    GameObject BalleActuelle;
+    [SerializeField]
+    GameObject Chargeur;
 
+    [SerializeField]
+    GameObject Douille;
+
+    GameObject BalleActuelle;
+    GameObject ChargeurActuel;
+    GameObject DouilleActuelle;
+    GameObject AncienChargeur;
+
+    int BalleDansChargeur;
+
+    [SerializeField]
+    int BalleDansChargeurMax = 10;
+
+    float TempsRestantAvantRéinsertion;
+    float TempsVouluAvantRéinsertion = 1;
+
+    [SerializeField]
+    float Puissance = 10;
 
     // Use this for initialization
     void Start()
     {
-        BalleActuelle = Instantiate(Balle, gameObject.transform);
+        TempsRestantAvantRéinsertion = Mathf.Infinity;
+        BalleDansChargeur = BalleDansChargeurMax;
+        BalleActuelle = Instantiate(Balle, gameObject.transform.position, gameObject.transform.rotation);
+        ChargeurActuel = Instantiate(Chargeur, gameObject.transform);
+        DouilleActuelle = Instantiate(Douille, gameObject.transform.position, gameObject.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(BalleActuelle.transform.position);
+        if (BalleDansChargeur == 0)
+        {
+            Debug.Log("Rechargement");
+            ChargeurActuel.GetComponent<Rigidbody>().useGravity = true;
+            ChargeurActuel.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0));
+            Debug.Log("Chargeur retiré");
+            TempsRestantAvantRéinsertion = TempsVouluAvantRéinsertion + Time.realtimeSinceStartup;
+            BalleDansChargeur = BalleDansChargeurMax;
+        }
+        if (Time.realtimeSinceStartup >= TempsRestantAvantRéinsertion)
+        {
+            ChargeurActuel = Instantiate(Chargeur, gameObject.transform.position, gameObject.transform.rotation);
+            Debug.Log("Chargeur réinséré");
+            TempsRestantAvantRéinsertion = Mathf.Infinity;
+        }
+       
+    }
+    void OnMouseDown()
+    {
+        BalleActuelle.GetComponent<Rigidbody>().AddForce(new Vector3(Puissance, 0, 0), ForceMode.Impulse);
+        DouilleActuelle.GetComponent<Rigidbody>().AddForce(new Vector3(Puissance, 0, 0), ForceMode.Impulse);
+        BalleDansChargeur -= 1;
+        Debug.Log(BalleDansChargeur);
+        BalleActuelle = Instantiate(Balle, gameObject.transform.position, gameObject.transform.rotation);
+        DouilleActuelle = Instantiate(Douille, gameObject.transform.position, gameObject.transform.rotation);
     }
 }
